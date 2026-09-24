dcc6b6a8b6392e2e1dcde36039f9fc2759ba8866
thanuja <ugcthanujasanjukasilva@gmail.com>
2026-09-24 12:18:33 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     std::cout << "---------------------------------------" << std::endl;
     std::cout << "Hello from Development Branch!" << std::endl;
-    std::cout << "---------------------------------------" << std::endl;
+    std::cout <<< "---------------------------------------" << std::endl;
     
     // Automated Test Condition
     std::cout << "Test Passed: Successfully built from the new branch." << std::endl;
