c250fdb0e15d97cabb58b52144afea428119d415
thanuja <ugcthanujasanjukasilva@gmail.com>
2026-09-24 10:40:07 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Jenkins! C++ Build Successful." << std::endl;
+    
+    int a = 15;
+    int b = 25;
+    int sum = a + b;
+    
+    std::cout << "The sum of " << a << " and " << b << " is: " << sum << std::endl;
+    
+    return 0;
+}
